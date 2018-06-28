1770ef37bbbc2be8e9d331a4c64c4bc67c6b5afd
Daymel Bonne <daymelbonne@gmail.com>
2018-06-27 21:05:22 -0500

    Sección 12 terminada

--- config/config.js
+++ config/config.js
@@ -1,1 +1,3 @@
 module.exports.SEED = 'este-es-un-seed-dificil';
+module.exports.GOOGLE_CLIENT_ID =
+  '824352941780-ahtaql970su2s17egjerk8v3tn0t9dmt.apps.googleusercontent.com';
--- routes/login.js
+++ routes/login.js
@@ -1,16 +1,97 @@
 var express = require('express');
 var bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
+const { SEED, GOOGLE_CLIENT_ID } = require('../config/config');
 
-var SEED = require('../config/config').SEED;
+// Google
+const { OAuth2Client } = require('google-auth-library');
+const client = new OAuth2Client(GOOGLE_CLIENT_ID);
 
 var app = express();
 var Usuario = require('../models/usuario');
 
+//=====================================
+// Autenticación Google
+//=====================================
+async function verify(token) {
+  const ticket = await client.verifyIdToken({
+    idToken: token,
+    audience: GOOGLE_CLIENT_ID // Specify the CLIENT_ID of the app that accesses the backend
+    // Or, if multiple clients access the backend:
+    //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+  });
+  const payload = ticket.getPayload();
+
+  return {
+    nombre: payload.name,
+    email: payload.email,
+    img: payload.picture,
+    google: true
+  };
+}
+
+app.post('/google', async (req, res) => {
+  var token = req.body.token;
+  var googleUser = await verify(token).catch(e => {
+    return res.status(403).json({
+      ok: false,
+      mensaje: 'Token no válido'
+    });
+  });
+
+  Usuario.findOne({ email: googleUser.email }, (err, usuario) => {
+    if (err) {
+      return res.status(500).json({
+        ok: true,
+        mensaje: 'Error al buscar usuario - login',
+        errors: err
+      });
+    }
+
+    if (usuario) {
+      if (usuario.google === false) {
+        return res.status(400).json({
+          ok: true,
+          mensaje: 'Debe de usar su autenticación local'
+        });
+      } else {
+        usuario.password = ':)';
+
+        generateAuthToken(usuario, res);
+      }
+    } else {
+      // Si el usuario no existe por correo
+      usuario = new Usuario({
+        nombre: googleUser.nombre,
+        email: googleUser.email,
+        img: googleUser.img,
+        password: ':)',
+        google: true
+      });
+
+      usuario.save((err, usuario) => {
+        if (err) {
+          return res.status(500).json({
+            ok: true,
+            mensaje: 'Error al crear usuario - google',
+            errors: err
+          });
+        }
+
+        generateAuthToken(usuario, res);
+      });
+    }
+  });
+});
+
+//=====================================
+// Autenticación local
+//=====================================
+
 app.post('/', (req, res) => {
   var body = req.body;
 
-  Usuario.findOne({ email: body.email }, (err, usuarioDB) => {
+  Usuario.findOne({ email: body.email }, (err, usuario) => {
     if (err) {
       return res.status(500).json({
         ok: false,
@@ -19,7 +100,7 @@ app.post('/', (req, res) => {
       });
     }
 
-    if (!usuarioDB) {
+    if (!usuario) {
       return res.status(400).json({
         ok: false,
         mensaje: 'Credenciales incorrectas - email',
@@ -27,7 +108,7 @@ app.post('/', (req, res) => {
       });
     }
 
-    if (!bcrypt.compareSync(body.password, usuarioDB.password)) {
+    if (!bcrypt.compareSync(body.password, usuario.password)) {
       return res.status(400).json({
         ok: false,
         mensaje: 'Credenciales incorrectas - password',
@@ -36,17 +117,20 @@ app.post('/', (req, res) => {
     }
 
     // Crear un token!!!
-    usuarioDB.password = ':)';
+    usuario.password = ':)';
 
-    var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 }); // 4 horas
-
-    res.status(200).json({
-      ok: true,
-      usuario: usuarioDB,
-      token: token,
-      id: usuarioDB._id
-    });
+    generateAuthToken(usuario, res);
   });
 });
+
+function generateAuthToken(usuario, res) {
+  var token = jwt.sign({ usuario: usuario }, SEED, { expiresIn: 14400 }); // 4 horas
+  res.status(200).json({
+    ok: true,
+    usuario,
+    token: token,
+    id: usuario._id
+  });
+}
 
 module.exports = app;
